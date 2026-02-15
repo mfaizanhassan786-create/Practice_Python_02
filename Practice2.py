c76e54bfeb5b11a3958fd292ae4f6eff1a834a69
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-02-15 22:38:21 +0500

    Practice Examples

--- Practice2.py
+++ Practice2.py
@@ -1,2 +1,17 @@
-print("Hello World")
-print("Hello Python")
+#Store following word meanings in a python dictionary :
+# Like that : 
+# table : "a piece of furniture with a flat top and one or more legs"
+# cat : "a small animal"
+
+
+mydict = {
+    "table" : ["a piece of furniture", "lists of facts and figures"],
+    "cat" : "a small animal"
+}
+print(mydict)
+
+#You are given a list of subjects for students.
+# Assume one classroom is required for 1 subject.
+#How many classrooms are needed by all the students.
+
+
