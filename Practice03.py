4c708668d699a1d784f216fff8aa455b7f3ee9a6
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-02-22 23:25:46 +0500

    Update

---
+++ Practice03.py
@@ -0,0 +1,18 @@
+# Hence in this file we will learn about the loops
+# In this we will learn about the for loop and while loop
+# And doing the practice of it
+
+# Let's Practice the loops:
+
+# Practice of the while loop:
+
+#Print Numbers from 1 to 100:
+
+i = 1
+while i <= 100:
+   
+   print(i)
+   i+= 1
+
+#Print numbera 100 to 1.
+#    
