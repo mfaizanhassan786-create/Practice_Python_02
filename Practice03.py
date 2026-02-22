449a2addd3c422298e97f7e2a0ccd842ac93dfa1
M.Faizan Hassan <mfaizanhassan786@gmail.com>
2026-02-22 23:34:55 +0500

    Update

--- Practice03.py
+++ Practice03.py
@@ -6,13 +6,35 @@
 
 # Practice of the while loop:
 
-#Print Numbers from 1 to 100:
+# #Print Numbers from 1 to 100:
 
-i = 1
-while i <= 100:
+# i = 1
+# while i <= 100:
    
-   print(i)
-   i+= 1
+#    print(i)
+#    i+= 1
 
-#Print numbera 100 to 1.
-#    
+# #Print numbera 100 to 1.
+
+# a = 100
+# while a >= 1:
+#     print(a)
+#     a-= 1
+
+# #Print the multiplication table of a number entered by the user.
+
+# num = int(input("Enter a number: "))  
+# i = 1
+# while i <= 10:
+#     print(num * i)
+#     i+= 1
+
+#Print the elements of the following list using a loop:
+# [1, 4, 9, 16, 25, 36, 49, 64, 81, 100] 
+
+my_list = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+i = 0
+while i <= 10:
+    print(my_list[i])
+    i+= 1
